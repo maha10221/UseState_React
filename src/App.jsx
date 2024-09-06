40f21e5cabaa499529b48f7ab6159b35ba6985d1
Mahesh10221 <117515189+Mahesh10221@users.noreply.github.com>
2024-09-06 14:27:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Click from './Click'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      {/* <h1 className='head'>Form Events</h1> */}
+        <Click />
+    </div>
+  )
+}
+
+export default App
